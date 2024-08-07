6004a0f60ad0e3b9b51cf0def2ec754b78ed1284
Laurent <lo.rail@proton.me>
2024-08-07 12:59:07 +0200

    modif comment zone

--- components/CommentZone.js
+++ components/CommentZone.js
@@ -1,59 +1,38 @@
 import styles from '../styles/CommentZone.module.css';
 import { useState } from 'react';
 import { urlBackend } from '../assets/varGlobal';
+import { useSelector } from 'react-redux'
+
 
 function CommentZone({ artwork }) {
-    const owner = 'joe';
-    const commentsFake = [
-        { alice: "I love the colors!" },
-        { bob: "Amazing details!" },
-        { joe: "Thanks for the feedback!" },
-        { charlie: "Beautiful composition!" },
-        { alice: "Inspiring work!" },
-        { joe: "Appreciate the support!" },
-        { bob: "Very creative!" },
-        { charlie: "Impressive technique!" },
-        { joe: "Thank you all!" },
-        { alice: "Stunning piece!" },
-        { bob: "Fantastic job!" },
-        { joe: "Glad you liked it!" },
-        { charlie: "Outstanding!" },
-        { alice: "Brilliant work!" },
-        { joe: "Thank you so much!" },
-        { bob: "Incredible!" },
-        { charlie: "Magnificent!" },
-        { joe: "Appreciate your kind words!" },
-        { alice: "Awesome artwork!" }
-    ];
-    const comments = artwork.comments || [];
+    const owner = useSelector((state) => state.user.value.username)
+    const comments = artwork.comments
 
-
-    const commentElements = comments.map((comment, index) => {
-        const username = Object.keys(comment)[0];
-        const commentText = comment[username];
-        const [comments, setComments] = useState([]);
-
-
-        return (
-            <div
-                key={index}
-                className={username === owner ? styles.commentOwner : styles.comment}
-            >
-                <strong>{username}:</strong> {commentText}
-            </div>
-        );
-    });
+    console.log(artwork)
+    // const commentsList = comments.map((comment, index) => {
+    //     const username = Object.keys(comment)[0];
+    //     const commentText = comment[username];
+    //     return (
+    //         <div
+    //             key={index}
+    //             className={username === owner ? styles.commentOwner : styles.comment}
+    //         >
+    //             <strong>{username}:</strong> {commentText}
+    //         </div>
+    //     );
+    // });
+    // console.log(commentsList)
 
     return (
         <div >
             <div className={styles.commentArea}>
-                {commentElements}
+                {/* {commentElements} */}
             </div>
             <div className={styles.newComment}>
                 <textarea placeholder="Your comment!" id="yourComment" className={styles.yourComment} />
-                <button id="send" className={`button ${styles.send}`}>
+                {/* <button id="send" className={`button ${styles.send}`} onClick={()=>send()}>
                     Send
-                </button>
+                </button> */}
             </div>
         </div>
     );
