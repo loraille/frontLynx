4485b8d3a05e48630a5e07834a7cf7458e7b530f
Laurent <lo.rail@proton.me>
2024-08-14 12:09:05 +0200

    sign in sin up style finish

--- components/SignIn.js
+++ components/SignIn.js
@@ -3,21 +3,29 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { login } from '../reducers/user';
 import { urlBackend } from '../assets/varGlobal'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 function SignIn({ isOpen, onClose }) {
     if (!isOpen) return null;
 
-    const [signUpUsername, setSignUpUsername] = useState('');
-    const [signUpPassword, setSignUpPassword] = useState('');
+    const [signInUsername, setSignInUsername] = useState('');
+    const [signInPassword, setSignInPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
 
     const dispatch = useDispatch();
 
     const handleConnection = () => {
-        console.log(signUpUsername, signUpPassword);
+        console.log(signInUsername, signInPassword);
         fetch(`${urlBackend}/users/signin`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ username: signUpUsername, password: signUpPassword }),
+            body: JSON.stringify({ username: signInUsername, password: signInPassword }),
         }).then(response => response.json())
             .then(data => {
                 if (data.result) {
@@ -25,19 +33,65 @@ function SignIn({ isOpen, onClose }) {
                     onClose();
                 } else {
                     console.warn("something went wrong", data.error);
+                    setErrorMessage(data.error);
                 }
             });
     };
+    const handleClickShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
 
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
     return (
         <div className={styles.overlay} onClick={onClose} style={{ zIndex: 1300 }}>
             <div className={styles.modal} onClick={(e) => e.stopPropagation()} style={{ zIndex: 1301 }}>
                 <p className="title">Enter in the place!</p>
-                <div>
-                    <div><input type="text" placeholder="Username" id="signUpUsername" className={styles.inputField} onChange={(e) => setSignUpUsername(e.target.value)} value={signUpUsername} /></div>
-                    <div><input type="password" placeholder="Password" id="signUpPassword" className={styles.inputField} onChange={(e) => setSignUpPassword(e.target.value)} value={signUpPassword} /></div>
-                    <button id="register" className={`button ${styles.register}`} onClick={handleConnection}>Sign In</button>
-                </div>
+                <Box
+                    component="form"
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        color: 'white',
+                        '& > :not(style)': { m: 1, width: '25ch' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <TextField
+                        id="outlined-basic-username"
+                        label="Username"
+                        variant="outlined"
+                        className="customTextField"
+                        onChange={(e) => setSignInUsername(e.target.value)}
+                        value={signInUsername}
+                    />
+                    <TextField
+                        id="outlined-basic-password"
+                        label="Password"
+                        variant="outlined"
+                        type={showPassword ? 'text' : 'password'}
+                        className="customTextField"
+                        onChange={(e) => setSignInPassword(e.target.value)}
+                        value={signInPassword}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                    >
+                                        {!showPassword ? <VisibilityOff className={styles.showPasswordOff} /> : <Visibility className={styles.showPassword} />}
+                                    </IconButton>
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                </Box>
+                {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>}
+                <button id="register" className={`button ${styles.register}`} onClick={handleConnection}>Sign In</button>
             </div>
         </div>
     );
--- components/Signup.js
+++ components/Signup.js
@@ -1,8 +1,15 @@
-import styles from '../styles/Signup.module.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import { useDispatch } from 'react-redux';
 import { login } from '../reducers/user';
-import { urlBackend } from '../assets/varGlobal'
+import { urlBackend } from '../assets/varGlobal';
+import styles from '../styles/Signup.module.css';
+
 
 function Signup({ isOpen, onClose }) {
     if (!isOpen) return null;
@@ -10,67 +17,111 @@ function Signup({ isOpen, onClose }) {
     const [signUpUsername, setSignUpUsername] = useState('');
     const [signUpEmail, setSignUpEmail] = useState('');
     const [signUpPassword, setSignUpPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
 
     const dispatch = useDispatch();
 
     const handleRegister = () => {
+        // Test email format
+        const emailPattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+        if (!emailPattern.test(signUpEmail)) {
+            setErrorMessage('Invalid email format');
+            return;
+        }
+        const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm
+        if (!passwordPattern.test(signUpPassword)) {
+            setErrorMessage('Invalid Password, example: Lynx777!');
+            return;
+        }
+
         console.log('Registering user:', { signUpUsername, signUpEmail, signUpPassword });
         fetch(`${urlBackend}/users/signup`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email: signUpEmail, username: signUpUsername, password: signUpPassword }),
-        }).then(response => response.json())
+        })
+            .then(response => response.json())
             .then(data => {
                 if (data.result) {
-                    console.log(data)
+                    console.log(data);
                     dispatch(login({ username: data.userInfo.username, userId: data.userInfo.email, token: data.userInfo.token }));
                     console.log("Bienvenito", data.userInfo.username);
                     onClose(); // Close the modal
                 } else {
-                    console.warn("something went wrong", data.error);
+                    console.warn("Something went wrong", data.error);
+                    setErrorMessage(data.error);
                 }
             });
     };
 
+    const handleClickShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
     return (
-        <div className={styles.overlay} onClick={onClose} style={{ zIndex: 1300 }} >
-            <div className={styles.modal} onClick={(e) => e.stopPropagation()} style={{ zIndex: 1301 }} >
+        <div className={styles.overlay} onClick={onClose} style={{ zIndex: 1300 }}>
+            <div className={styles.modal} onClick={(e) => e.stopPropagation()} style={{ zIndex: 1301 }}>
                 <p className="title">Create your account</p>
-                <div className={styles.form}>
-                    <div>
-                        <input
-                            type="text"
-                            placeholder="Email"
-                            id="signUpEmail"
-                            className={styles.inputField}
-                            onChange={(e) => setSignUpEmail(e.target.value)}
-                            value={signUpEmail}
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="text"
-                            placeholder="Username"
-                            id="signUpUsername"
-                            className={styles.inputField}
-                            onChange={(e) => setSignUpUsername(e.target.value)}
-                            value={signUpUsername}
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="password"
-                            placeholder="Password"
-                            id="signUpPassword"
-                            className={styles.inputField}
-                            onChange={(e) => setSignUpPassword(e.target.value)}
-                            value={signUpPassword}
-                        />
-                    </div>
-                    <button id="register" className={`button ${styles.register}`} onClick={handleRegister}>
-                        Sign up
-                    </button>
-                </div>
+                <Box
+                    component="form"
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        color: 'white',
+                        '& > :not(style)': { m: 1, width: '25ch' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <TextField
+                        id="outlined-basic-email"
+                        label="Email"
+                        variant="outlined"
+                        className="customTextField"
+                        onChange={(e) => setSignUpEmail(e.target.value)}
+                        value={signUpEmail}
+                    />
+                    <TextField
+                        id="outlined-basic-username"
+                        label="Username"
+                        variant="outlined"
+                        className="customTextField"
+                        onChange={(e) => setSignUpUsername(e.target.value)}
+                        value={signUpUsername}
+                    />
+                    <TextField
+                        id="outlined-basic-password"
+                        label="Password"
+                        variant="outlined"
+                        type={showPassword ? 'text' : 'password'}
+                        className="customTextField"
+                        onChange={(e) => setSignUpPassword(e.target.value)}
+                        value={signUpPassword}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                    >
+                                        {!showPassword ? <VisibilityOff className={styles.showPasswordOff} /> : <Visibility className={styles.showPassword} />}
+                                    </IconButton>
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                </Box>
+
+                {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>}
+                <button id="register" className={`button ${styles.register}`} onClick={handleRegister}>
+                    Sign up
+                </button>
             </div>
         </div>
     );
