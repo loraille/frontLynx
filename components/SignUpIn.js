e74217c9baabbca24810156ce60ab303abb588ba
nasserm1 <nm01dev@outlook.com>
2024-08-15 16:17:37 +0200

    fix signUpIn modal close

--- components/SignUpIn.js
+++ components/SignUpIn.js
@@ -1,14 +1,14 @@
 import styles from '../styles/SignUpin.module.css';
 
-import SignIn from './SignIn';
-import Signup from './Signup';
+/* import SignIn from './SignIn';
+import Signup from './Signup'; */
 import { useState } from 'react';
 
 import { GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from "jwt-decode";
 
 
-function SignUpIn({ isOpen, onClose }) {
+function SignUpIn({ isOpen, onClose, onOpenModal}) {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [modalType, setModalType] = useState('');
 
@@ -23,9 +23,20 @@ function SignUpIn({ isOpen, onClose }) {
         setIsModalOpen(true);
         console.log("type isModalOpen", type, isModalOpen);
     };
+
+ 
     const handleCloseModal = () => {
+        console.log("####SUI handleCloseModal");
         setIsModalOpen(false);
         setModalType('');
+    };
+
+   // DEBUG 
+    const setTypeOfModal = (type) => {
+        console.log("b4    type:",type,"modalType:", modalType,"isModalOpen:",isModalOpen );
+        setIsModalOpen(true);
+        setModalType(type);
+        console.log("after type:",type,"modalType:", modalType,"isModalOpen:",isModalOpen );
     };
 
     return (
@@ -34,10 +45,10 @@ function SignUpIn({ isOpen, onClose }) {
             <div className={styles.modal} onClick={(e) => e.stopPropagation()} style={{ zIndex: 1301, color: 'white' }}>
                 <div className={styles.container}>
                     <span className='title'>Welcome in the place!</span>
-                    <button id="register" className={`button ${styles.register}`} onClick={() => handleOpenModal('signin')}>Sign In</button>
-                    Hey please Sign Up for such actions
-                    <button id="register" className={`button ${styles.register}`} onClick={() => handleOpenModal('signup')}>Sign Up</button>
-
+                    Hey please Sign In for such actions
+                    <button id="register" className={`button ${styles.register}`} onClick={() => { setTypeOfModal('signin'); onOpenModal('signin')} }>Sign In</button>
+                    Not account ? Sign Up Now !  
+                    <button id="register" className={`button ${styles.register}`} onClick={() => { setTypeOfModal('signup'); onOpenModal('signup')} }>Sign Up</button>
                     {/*------------------------------------------------- CONNEXION GOOGLE------------------------------- */}
                     {/*  <div className={styles.divider}></div>
                     Or continue with google :
@@ -58,8 +69,8 @@ function SignUpIn({ isOpen, onClose }) {
                         </div>
                     } */}
                 </div>
-                {modalType === 'signup' && <Signup isOpen={isModalOpen} onClose={handleCloseModal} />}
-                {modalType === 'signin' && <SignIn isOpen={isModalOpen} onClose={handleCloseModal} />}
+                {/* {modalType === 'signup' && <Signup isOpen={isModalOpen} onClose={handleCloseModal} />}
+                {modalType === 'signin' && <SignIn isOpen={isModalOpen} onClose={handleCloseModal} />} */}
             </div>
         </div >
 
