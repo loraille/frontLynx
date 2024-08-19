aa4372bec2818cb4f5a8a1884b5b28ebc0a6fc28
Laurent <lo.rail@proton.me>
2024-08-19 20:09:16 +0200

    Home.js defilement

--- components/ArtworkCard.js
+++ components/ArtworkCard.js
@@ -1,29 +1,32 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
-import styles from '../styles/ArtworkCard.module.css';
+import styles from "../styles/ArtworkCard.module.css";
 
 const ArtworkCard = ({ artwork }) => {
   //let artistName = "Gregor S";
   //let artistName = useSelector((state) => state.user.value.username)
   let artistName = artwork.username;
-  console.log(artwork.url, artistName);
 
-
-return (
-  <div className={styles.artworkCard}>
-    <Link href={{ pathname: '/artworkView', query: { id: artwork._id } }}>
-      <img src={artwork.url} alt={artwork.title} className={styles.artworkImage} />
-    </Link>
-    <div className={styles.infos}>
-      <Link href={{ pathname: '/user', query: { username: artwork.uploader } }}>
-        <a className={styles.artistName}>{artwork.uploader}</a>
+  return (
+    <div className={styles.artworkCard}>
+      <Link href={{ pathname: "/artworkView", query: { id: artwork._id } }}>
+        <img
+          src={artwork.url}
+          alt={artwork.title}
+          className={styles.artworkImage}
+        />
       </Link>
-      <div className={styles.cardTitle}>{artwork.title}</div>
+      <div className={styles.infos}>
+        <Link
+          href={{ pathname: "/user", query: { username: artwork.uploader } }}
+        >
+          <a className={styles.artistName}>{artwork.uploader}</a>
+        </Link>
+        <div className={styles.cardTitle}>{artwork.title}</div>
+      </div>
     </div>
-  </div>
-);
-
+  );
 };
 
 export default ArtworkCard;
