2345b1634f52221fda325daa516b29c44886950d
Laurent <lo.rail@proton.me>
2024-08-23 10:40:54 +0200

    modif Image addartwork.js collectionCard.js

--- components/AddArtwork.js
+++ components/AddArtwork.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "next/link";
+import Image from "next/image";
 import styles from "../styles/AddArtwork.module.css";
 
 const AddArtwork = ({ artwork, onClick }) => {
@@ -8,12 +8,16 @@ const AddArtwork = ({ artwork, onClick }) => {
 
   return (
     <div className={styles.artworkCard}>
-      <img
-        src={artwork.url}
-        alt={artwork.title}
-        className={styles.artworkImage}
-        onClick={onClick}
-      />
+      <div className={styles.artworkImageContainer}>
+        <Image
+          src={artwork.url}
+          alt={artwork.title}
+          layout="fill"
+          objectFit="cover"
+          className={styles.artworkImage}
+          onClick={onClick}
+        />
+      </div>
       <div className={styles.infos}>
         <div className={styles.cardTitle}>{artwork.title}</div>
       </div>
--- components/CollectionsCard.js
+++ components/CollectionsCard.js
@@ -1,19 +1,25 @@
 import React from "react";
 import styles from "../styles/CategoriesCard.module.css";
 import Link from "next/link";
+import Image from "next/image";
 
 const CollectionsCard = ({ uploader, collectionName, image_url, artworks }) => {
+  //check all required props are defined
+  if (!uploader || !collectionName || !image_url) {
+    return null;
+  }
+
   return (
     <div className={styles.categoryCard}>
       <Link
         href={`/artworksDisplay/?fromLink=collection&toDisplay=${collectionName}&uploader=${uploader}`}
       >
         <a>
-          <img
+          <Image
             src={image_url}
             alt={collectionName}
-            layout="fill"
-            objectFit="cover"
+            width={380}
+            height={380}
             className={styles.collectionImage}
           />
         </a>
